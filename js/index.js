e229885b0122130f95b7b30a384b969602d7d9d9
Mysti Dyse <mystidyse@gmail.com>
2019-08-09 17:29:34 -0700

    updated inddex.js to include instructions from README.md as well as example code for how to add event listeners. I also included the 10 event listeners I decided to use

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// Instructions! for Task 2
+// * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners] using your creativity to make the Fun Bus site more interactive. 
+// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+
+// adding event listener ex.
+// element.addEventListener('click', callback);
+// element.addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
+
+// the TEN event listeners I will be using: resize, scroll, fullscreenchange, keydown, keyup, contextmenu,  dblclick, mouseleave, select, wheel
+
