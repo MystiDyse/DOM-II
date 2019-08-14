67070cb0d788b70df032a723222114453f6eec6b
Mysti Dyse <mystidyse@gmail.com>
2019-08-13 18:01:52 -0700

    task 1-3 complete. through 1on1 i was able to figure out the reason my code wacnt working. i need to spend more time on how to properly use queryselector and getelementsbyclassname

--- js/index.js
+++ js/index.js
@@ -8,5 +8,75 @@
 // element.addEventListener('click', callback);
 // element.addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
 
-// the TEN event listeners I will be using: resize, scroll, fullscreenchange, keydown, keyup, contextmenu,  dblclick, mouseleave, select, wheel
+
+let navTAg = document.querySelector('.logo-heading');
+navTAg.addEventListener('mouseout', (event) => {
+    event.currentTarget.style.color = 'orange'
+})
+
+const contentSection = document.querySelector('.content-section');
+contentSection.addEventListener('contextmenu', (event) => {
+    alert('Hey whatcha Doing!? This is Mine!')
+    preventDefault()
+})
+
+document.addEventListener('keypress', (event) => {
+    if (event.key === 'f') {
+        alert('F is for Fun Bus')
+    }
+})
+
+const sandPit = document.getElementsByClassName("sand")[0]
+sandPit.addEventListener('click', (event) => {
+    event.target.style.border = 'solid pink 3px'
+})
+
+const images = document.getElementsByClassName('img-content');
+for (let img of images) {
+    img.addEventListener('mouseenter', () => {
+        event.target.style.border = 'dashed blue 5px'
+    })
+}
+
+
+const fluid = document.querySelector('.text-content');
+fluid.addEventListener('dblclick', (event) => {
+    event.currentTarget.style.border = 'solid green 3px';
+    stopPropogation()
+})
+
+const input = document.getElementsByClassName('content-destination')[0];
+input.addEventListener('mouseenter', (event) => {
+    event.style.backgroundColor = 'grey';
+})
+
+const container = document.querySelector('.destination')
+container.addEventListener('mouseenter', bkgAndBorder)
+function bkgAndBorder(event) {
+    event.currentTarget.style.backgroundColor = 'purple';
+    event.currentTarget.style.borderColor = 'green';
+   }
+
+const buttons = document.querySelectorAll('.btn')
+for (let button of buttons) {
+	button.addEventListener("click", () => {
+		button.style.color = "#17A2B8";
+		button.style.backgroundColor = "#FFF";
+	});
+}
+// Collapse
+
+
+
+// Message Input
+
+
+// Message Timothy Shores 
+
+
+window.addEventListener('resize', (event) =>{
+    console.log('window is resizing')
+})
+
+
 
